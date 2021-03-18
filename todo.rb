21ba4544391fb309df19a2c9f937a80b751dc593
Rajanayaki <rajanayaki4444@gmail.com>
2021-03-18 20:30:34 +0530

    Assessment Completed quit

--- todo.rb
+++ todo.rb
@@ -1,3 +1,96 @@
-puts " What's in your todo?"
-todo = gets
-puts "so,your todo is: #{todo * 10} "
+require "date"
+$stdout.sync = true
+
+class Todo
+  def initialize(text, due_date, completed)
+    @text = text
+    @due_date = due_date
+    @completed = completed
+  end
+
+  def overdue?
+    @due_date < Date.today
+  end
+
+  def due_today?
+    @due_date == Date.today
+  end
+
+  def due_later?
+    @due_date > Date.today
+  end
+
+  def text
+    @text
+  end
+
+  def due_date
+    @due_date
+  end
+
+  def completed
+    @completed
+  end
+
+  def to_displayable_string
+    if completed
+      print "[X]"
+    else
+      print "[]"
+    end
+    if due_date != Date.today
+      puts "#{text} #{due_date}"
+    else
+      puts "#{text}"
+    end
+  end
+end
+
+class TodosList
+  def initialize(todos)
+    @todos = todos
+  end
+
+  def add(todo)
+    @todos.push(todo)
+  end
+
+  def overdue
+    TodosList.new(@todos.filter { |todo| todo.overdue? })
+  end
+
+  def due_today
+    TodosList.new(@todos.filter { |todo| todo.due_today? })
+  end
+
+  def due_later
+    TodosList.new(@todos.filter { |todo| todo.due_later? })
+  end
+
+  def to_displayable_list
+    @todos.each { |todo| todo.to_displayable_string }
+  end
+end
+
+date = Date.today
+todos = [
+  { text: "Submit assignment", due_date: date - 1, completed: false },
+  { text: "Pay rent", due_date: date, completed: true },
+  { text: "File taxes", due_date: date + 1, completed: false },
+  { text: "Call Acme Corp.", due_date: date + 1, completed: false },
+]
+todos = todos.map { |todo|
+  Todo.new(todo[:text], todo[:due_date], todo[:completed])
+}
+todos_list = TodosList.new(todos)
+todos_list.add(Todo.new("Service vehicle", date, false))
+puts "My Todo-list\n\n"
+puts "Overdue\n"
+todos_list.overdue.to_displayable_list
+puts "\n\n"
+puts "Due Today\n"
+todos_list.due_today.to_displayable_list
+puts "\n\n"
+puts "Due Later\n"
+todos_list.due_later.to_displayable_list
+puts "\n\n"
