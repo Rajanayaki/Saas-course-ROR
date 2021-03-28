930899d9444d53bbf7145a55a52d32bad6b119e5
Rajanayaki S <45528670+Rajanayaki@users.noreply.github.com>
2021-03-28 20:54:10 +0530

    Update complete-todo.rb

--- L4-ActiveRecord/complete-todo.rb
+++ L4-ActiveRecord/complete-todo.rb
@@ -5,5 +5,6 @@ Todo.show_list
 puts "Which todo do you want to mark as complete? (Enter id): "
 todo_id = gets.strip.to_i
 todo = Todo.mark_as_complete!(todo_id)
-puts todo.class
+todo.update(completed: true)
 todo.to_displayable_string
+
